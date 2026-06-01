364d968bebf6812909f25fb8a26d8eadfa1eb1ef
Naim <naimul.prodhan81@hotmail.com>
2026-06-01 20:57:32 +0600

    Refactor: Menucode refactred byuser role type. Help and anguage menus are now in view component.

---
+++ Main.WebAppCore/ViewComponents/CompanyMenuViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Main.WebAppCore;
+
+public class CompanyMenuViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View();
+    }
+}
