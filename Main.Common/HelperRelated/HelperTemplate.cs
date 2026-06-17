1d528a76b122eb13e61524aa0675aaca42802035
Naim <naimul.prodhan81@hotmail.com>
2026-06-17 19:26:38 +0600

    Remove HelperTemplate.cs, add tasks and new helper class
    - Exclude HelperTemplate.cs and ImageHelper.cs from build in csproj
    - Add TaskRajwan1.txt with Google Drive folder/image tasks
    - Add new HelperTemplate.cs with template post limit logic

---
+++ Main.Common/HelperRelated/HelperTemplate.cs
@@ -0,0 +1,27 @@
+﻿using Main.Common.Enums;
+
+namespace Main.Common.HelperRelated;
+
+public static class HelperTemplate
+{
+    public static Dictionary<int,int> GetTemplatePostLimit ( )
+    {
+        Dictionary<int, int> listTemplatePostLimit = new Dictionary<int, int> ()
+        {
+            { (int) EnumPanelTemplate.ProductQuard, 4 },
+            { (int) EnumPanelTemplate.ProductDouble, 2 },
+            { (int) EnumPanelTemplate.ProductTriangle, 3 },
+            { (int) EnumPanelTemplate.ProductSixer, 6 },
+            { (int) EnumPanelTemplate.AdminBannerCarousel, 4 },
+            { (int) EnumPanelTemplate.AdminSingleBanner, 1 }
+        };
+
+        return listTemplatePostLimit;
+    }
+
+    public static int FindTemplatePostLimit ( int enumPanelTemplate )
+    {
+        Dictionary<int,int>  dictLimit = GetTemplatePostLimit();
+        return dictLimit[enumPanelTemplate];
+    }
+}
