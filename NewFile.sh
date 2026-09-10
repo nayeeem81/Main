5b3afc67002047f480afafd57e64e597ff4da324
Naim <naimul.prodhan81@hotmail.com>
2026-09-10 06:03:32 +0600

    Remove controllers, scripts, and site assets
    Deleted main controllers, error handler, shell scripts, view start config, CSS, and JS files to clean up project structure.

--- NewFile.sh
+++
@@ -1,9 +0,0 @@
-git fetch origin
-git checkout master
-git pull origin master
-git branch backup-master
-
-# Switch to master and merge preferring the branch changes
-git checkout master
-git pull origin master
-git merge --no-ff -X theirs "$BRANCH" -m "Merge $BRANCH into master (prefer branch)"
--- wwwroot/js/site.js
+++
@@ -1,5 +0,0 @@
-// Basic site JavaScript
-document.addEventListener('DOMContentLoaded', function () {
-    // Placeholder for site scripts
-    console.log('Site scripts loaded');
-});
